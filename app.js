9b566e2d5bc116d591e75f1f58f751cc81566ef2
droza0 <droza0@users.noreply.github.com>
2019-09-26 11:38:48 -0400

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const layout = require("./views/layout")
+const {db} = require('./models')
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan)
+
+db.authenticate().
+  then(() => {
+    console.log('connected to the database');
+  })
 
 app.get("/", function(req, res) {
-  res.send("Hello World!");
+  res.send(layout(""));
 });
 
 app.listen(3000);
